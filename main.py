0b97464d0c5c1daf980d9293cf972c9a128eb954
Abhay Jyot <abhayjyot.b@gmail.com>
2022-09-10 03:01:16 +0530

    Updated

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from git import Repo
 from git import Git
 import git
 
-username = "Abhay-Jyot7" #Your github username here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
-password = "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" #Your github password here (Use auth token for password )
+username = "" #Your github username here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
+password = "" #Your github password here (Use auth token for password )
 number = int()
 commit_message = ""
 
@@ -26,35 +26,26 @@ def commit_files(url , number , commit_message):
 
     elif number == 1:#Case 3: If the file is already present in github and we want to update it in the main branch
         repo = Repo(repo_dir)
-        repo.git.add([file_name])
+        repo.git.add(all=True) #Add the file to the repo
         repo.index.commit(commit_message)
         repo.remotes.origin.push(refspec='HEAD:refs/heads/main', force=True)
 
 
     return     
 
-def pull_files(url, number):
+def pull_files(url):
     repo_dir =  (r'C:\Users\abc\Desktop\newtest111') #Folder that is to be pulled from github
-    file_name = os.path.join(repo_dir ,'work') #File/Folder inside the folder that is to be pulled from github i.e. in our case repo_dir
     #repo = Repo(repo_dir)
+
     repo = Repo.init(repo_dir , env={"GIT_SSH_COMMAND": 'ssh -i ~/.ssh/id_rsa'})
     repo.create_remote("origin", url=url)
-    if number == 0:
-        repo.git.pull(url) #Pulling from master branch
-    elif number == 1:
-        repo.git.pull(url, "master")
-    elif number == 2:
-        repo.git.pull(url, "qa")
-
-
+    repo.git.pull(url) #Pulling from master branch
 
     return
 
 
 if __name__ == '__main__': 
     
-    #commit_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim1.git", number= 0, commit_message = "Message Check")#Use the url of the repository created in github   
-    #commit_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim2.git", number= 0, commit_message = "Message Check")#Use the url of the repository created in github     
-    #commit_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim3.git", number= 0, commit_message = "Message Check")#Use the url of the repository created in github     
-    pull_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim2.git", number= 0)
+    #commit_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim1.git", number= 0, commit_message = "Message Check")#Use the url of the repository created in github       
+    pull_files(f"https://{username}:{password}@github.com/Abhay-Jyot7/munim2.git")
 
